82b8ef602a432d3667cab011a3299df9c067b534
jakaria242 <jakaria.dev242@gmail.com>
2023-10-08 15:43:40 +0600

    innovate 03 hlaf project

---
+++ js/tailwind.config.js
@@ -0,0 +1,37 @@
+
+
+    tailwind.config = {
+      theme: {
+        extend: {
+          colors: {
+            black: '#2C2643',
+          },
+        spacing: {
+            menuGap: '72px',
+            menuBtnGap: '17px',
+            bannerpt: '195px',
+            bannerpb: '558px',
+            'bnrh1Width': '796px',
+            'bnrpWidth': '467px',
+          },
+        maxWidth: {
+            'container': '1410px',
+          },
+        fontFamily: {
+            'dmSans': ['DM Sans', 'sans-serif'],
+            'chivo': ['Chivo','sans-serif'],
+          },
+         backgroundImage: {
+            'bannerImg': "url('images/banner.png')",
+         },
+        fontSize: {
+            'bigh1': '56px',
+            'middumh2': '46px',
+          },
+        lineHeight: {
+             'bigLine': '64px',
+             'middamLine': '64px',
+          }
+        }
+      }
+    }
